65e908c8b24f7cb8ae94ccb9fc9ddb736a1903e5
tom <tom.hervieu@gmail.com>
2024-08-04 14:45:47 -0400

    init tauri

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { invoke } from '@tauri-apps/api'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+invoke('greet', {name:'World'})
+  .then(res => console.log(res));
