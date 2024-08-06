989a0b0237ee4173f704a3b750c739f59ea2f643
tom <tom.hervieu@gmail.com>
2024-08-05 23:35:28 -0400

    add react-flow lib and hookup sample graph

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ReactFlow } from '@xyflow/react'
+import '@xyflow/react/dist/style.css';
+ 
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +25,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div style={{ width: '50vw', height: '50vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is now: {count}
